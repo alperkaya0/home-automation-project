2c5625064b3ebc4a4d40dca0a3248b624869e0d8
meryemAhiskali <120027848+meryemAhiskali@users.noreply.github.com>
2023-07-05 21:25:11 +0300

    change update sql query to insert into

--- consumer/buttons-backend/buttons_emergency.php
+++ consumer/buttons-backend/buttons_emergency.php
@@ -11,8 +11,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $body = file_get_contents('php://input');
     echo json_encode($body);
     $isEmergency = json_decode($body)->isEmergency;
+    $currentDate = date('Y-m-d'); // Get the current date
+    $sql = "INSERT INTO user_option (date, name, _option, value) VALUES ('".$currentDate."', '".$_SESSION["username"]."', 'isEmergency', '".$isEmergency."');";
 
-    $sql = "UPDATE user_option SET value = '".$isEmergency."' WHERE name = '".$_SESSION["username"]."' AND _option = 'isEmergency';";
     $result = mysqli_query($conn, $sql);    
 } else {
     $sql = "SELECT value FROM `user_option` WHERE name = '".$_SESSION["username"]."' AND _option = 'isEmergency'; ";
