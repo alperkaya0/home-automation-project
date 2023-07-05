e7a5525c8d04747f3b2ce8f3502b9ea29a14c96a
meryemAhiskali <120027848+meryemAhiskali@users.noreply.github.com>
2023-07-05 21:24:33 +0300

    change update to insert into

--- consumer/buttons-backend/buttons_ac.php
+++ consumer/buttons-backend/buttons_ac.php
@@ -11,8 +11,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $body = file_get_contents('php://input');
     echo json_encode($body);
     $isAcOn = json_decode($body)->isAcOn;
+    $currentDate = date('Y-m-d'); // Get the current date
+    $sql = "INSERT INTO user_option (date, name, _option, value) VALUES ('".$currentDate."', '".$_SESSION["username"]."', 'isAcOn', '".$isAcOn."');";
 
-    $sql = "UPDATE user_option SET value = '".$isAcOn."' WHERE name = '".$_SESSION["username"]."' AND _option = 'isAcOn';";
     $result = mysqli_query($conn, $sql);    
 } else {
     $sql = "SELECT value FROM `user_option` WHERE name = '".$_SESSION["username"]."' AND _option = 'isAcOn'; ";
